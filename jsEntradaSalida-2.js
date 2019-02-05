8ff285e902be4e97aee6636fa5509aed4b0e0628
MauricioFloresGonales <47356611+MauricioFloresGonales@users.noreply.github.com>
2019-02-05 12:14:40 -0300

    entrada y salida primera clase
    mi primer contacto con la programación.

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{ 
+	var presionar = prompt("Por favor ingrese su nombre y apellido", "nombre");
+    alert(persona)+("nombre")
+
+
+
+
+}
+
