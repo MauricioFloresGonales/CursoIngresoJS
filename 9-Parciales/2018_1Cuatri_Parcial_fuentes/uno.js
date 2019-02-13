ed4e9fcf5d545f649fd0b4c72919f83977e245ab
alumno <alumno>
2019-02-13 09:33:43 -0300

    ejercicio de evaluacion sin terminar

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,26 @@
 
 function mostrar()
 {
+	var nombre1;
+	var nombre2;
+	var edad1;
+	var edad2;
+	var suma;
+	
+	nombre1= prompt("escriba su nombre y su apellido","nombre y apellido");
+	edad1= prompt("su edad es...","escriba su edad");
+	edad1= parseInt(edad1);
+
+	nombre2= prompt("esccriba su nombre y su apellido","nobre y apellido");
+	edad2= prompt("su edad es...","escriba su edad");
+	edad2= parseInt(edad2);
+
+	suma= (edad1 + edad2);
+
+	alert("ustedes son " + nombre1 + " , " + nombre2 + " y sus edades son " + edad1 + " , " edad2);
+
+//el alert no me da lo que pidio
+
+
 
 }
