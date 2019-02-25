1e84a5519e04550ed96d4103f94ec55c8487c420
MauriciofloresGonales <mlgf2000@hotmail.com>
2019-02-25 20:53:33 -0300

    adivina el numero incompleto

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -8,19 +8,55 @@ secreto del 1 al 100, en la pantalla del juego
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
 var numeroSecreto; 
-var contadorIntentos;
+//var contadorIntentos;
+var numeroIngresado;
+var mensaje;
+
+	numeroSecreto= Math.floor((Math.random() * 100) + 1);
+
+contadorIntentos= 0 ;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
 		//alert(numeroSecreto );
+
+	numeroIngresado= document.getElementById('numero').value;
+
 	
+	mensaje= numeroSecreto;
+
+
+	if(numeroIngresado<numeroSecreto)
+	{
+		mensaje= "falta…";
+	}else{
+
+		if(numeroIngresado>numeroSecreto)
+		{
+			mensaje= "se pasó…";
+		}else{
+			mensaje= "Usted es un ganador!!! y en solo X intentos";
+		}//if(numeroIngresado>numeroSecreto)
+	}//if(numeroIngresado<numeroSecreto)
+
+	alert(mensaje);
 
 }
 
 function verificar()
 {
+	var contadorIntentos;
 	
+	while(contadorIntentos>5)
+	{
+		alert("intenta de nuevo");
+		contadorIntentos = contadorIntentos + 1; 
+		
+	}
 	
+	document.getElementById('intentos').value =contadorIntentos;
 }
+
+ 
